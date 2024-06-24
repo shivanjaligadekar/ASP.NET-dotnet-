eddb421f8c900006dc3674ac43577accdfa3ffb8
Shivanjali Gadekar <145742159+shivanjaligadekar@users.noreply.github.com>
2024-06-24 18:32:17 +0530

    Add files via upload

---
+++ BusinessLogicLibrary/CustomerBAL.cs
@@ -0,0 +1,20 @@
+﻿namespace BusinessLogicLibrary
+{
+    public class CustomerBAL
+
+    {
+        private int _custid;
+        private string _custname;
+
+        public int CustomerId
+        {
+            get { return _custid; }
+            set { _custid = value; }
+        }
+        public string CustomerName
+        {
+            get { return _custname; }
+            set { _custname = value; }
+        }
+    }
+}
